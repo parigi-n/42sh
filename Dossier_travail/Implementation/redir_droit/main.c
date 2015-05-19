edfd28dbd67dcdf599f817a84bf13271f7bfcbe2
PELLEMOINE <m.pellemoine@gmail.com>
2015-05-19 16:29:56 +0200

    redir droite

---
+++ Dossier_travail/Implementation/redir_droit/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for main in /home/pellem_m/PSU_2014_42sh/Dossier_travail/Implementation/redir_droit
+** 
+** Made by Martin PELLEMOINE
+** Login   <pellem_m@epitech.net>
+** 
+** Started on  Tue May 19 16:29:06 2015 Martin PELLEMOINE
+** Last update Tue May 19 16:29:41 2015 Martin PELLEMOINE
+*/
+
+void		main(int ac, char **av)
+{
+  
+}
